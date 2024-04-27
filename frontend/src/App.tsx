6fbf20ab6a56591b4505ddb087141666a5523636
Arnaud <arnaud-buhl@orange.fr>
2024-04-27 20:02:39 +0200

    feat: router and app base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Project from './pages/project/Project';
+import Error404 from './pages/404/Error404';
+import Legals from './pages/legals/Legals';
+import './global.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <>
+        <Header/>
+        
+        <Routes>
+            <Route path='*' element={<Error404/>}/>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/project/:id' element={<Project/>}/>
+            <Route path='/legals' element={<Legals/>}/>
+        </Routes>
+        
+        <Footer/>
+    </>
 }
 
 export default App;
